b5a52bb3773621bd2d46da89fade8ce7fd178a17
Joanna Malysa <lawerna@gmail.com>
2014-04-25 12:20:47 +0200

    Updated slider - functionality

--- js/pttk_angular.js
+++ js/pttk_angular.js
@@ -1,37 +1,48 @@
-var pttkApp = angular.module('pttkApp', ['ngRoute']);
+var pttkApp = angular.module('pttkApp', ['ngRoute', 'ui.bootstrap']);
 
 // configure our routes
 pttkApp.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl : 'partials/home.html',
-            controller  : 'mainController'
-        })
-        .when('/imprezy', {
-            templateUrl : 'partials/imprezy.html',
-            controller  : 'imprezyController'
-        });
+  $routeProvider
+    .when('/', {
+      templateUrl : 'partials/home.html',
+      controller  : 'mainController'
+    })
+    .when('/imprezy', {
+      templateUrl : 'partials/imprezy.html',
+      controller  : 'imprezyController'
+    });
 });
 
-// create the controller and inject Angular's $scope
 pttkApp.controller('mainController', function($scope) {
-    $scope.message = 'Everyone come and see how good I look!';
+});
+
+
+pttkApp.controller('carouselController', function($scope, $filter) {
+  $scope.myInterval = 3000;
+  var slides = $scope.slides = [];
+  var stories = [
+    {'title': 'Rajd im. J. Kukuczki 2014',
+     'content': 'W dniu 17 maja odbędzie się Rajd Górski im. Jerzego Kukuczki, organizowany głównie przez oddział PTTK Wisła pod patronatem Pani Cecylii Kukuczki i m.in. UG Istebna',
+     'age': 1},
+    {'title': 'Rajd ku źródłom Wisły 2014',
+     'content': 'ODDZIAŁ PTTK "WISŁA" zaprasza na XI międzynarodowy rajd narciarsko-pieszy "Ku żródłom Wisły". Rajd odbędzie się 22 marca 2014r. na dwóch trasach. Serdecznie zapraszamy!',
+     'age': 3},
+    {'title': 'Nowy szlak turystyczny',
+     'content': 'Serdecznie zapraszamy na nowy szlak turystyczny autorstwa Eli Dorighi na terenie Wisly wytyczony',
+     'age': 2}
+  ];
+  
+  stories = $filter('orderBy')(stories, 'age');
+
+  for (var i=0; i<3; i++) {
+    slides.push({
+      title: stories[i].title,
+      content: stories[i].content
+    });
+    console.log(slides);
+  }
 });
 
 pttkApp.controller('imprezyController', function($scope) {
-    $scope.message = 'Look! I am an imprezy page.';
-});
-
-pttkApp.controller('StoryListCtrl', function ($scope) {
-  $scope.stories = [
-    {'title': 'Rajd im. J. Kukuczki 2014',
-     'text': 'W dniu 17 maja odbędzie się Rajd Górski im. Jerzego Kukuczki, organizowany głównie przez oddział PTTK Wisła pod patronatem Pani Cecylii Kukuczki i m.in. UG Istebna',
-     'age': 1},
-    {'title': 'Rajd ku źródłom Wisły 2014',
-     'text': 'ODDZIAŁ PTTK "WISŁA" zaprasza na XI międzynarodowy rajd narciarsko-pieszy "Ku żródłom Wisły". Rajd odbędzie się 22 marca 2014r. na dwóch trasach. Serdecznie zapraszamy!',
-     'age': 3},
-    {'title': 'Nowy szlak turystyczny',
-     'text': 'W jesieni ub. roku został na terenie Wisly wytyczony nowy szlak turystyczny autorstwa Eli Dorighi - naszej "świeżo upieczonej przewodniczki". Jest to przedmiotem jej pracy    magisterskiej. Poniżej przedstawiamy jej autorski opis szlaku i mapkę na której szlak zaznaczony jest kolorem ciemnoczerwonym. Zapraszamy wszystkich do przejscia ta trasą bo jest, naprawdę ciekawa.',
-     'age': 2}
-  ];
+  $scope.message = 'Look! I am an imprezy page.';
 });
